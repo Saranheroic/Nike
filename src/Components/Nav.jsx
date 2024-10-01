dcd110090b6955ee879b59b1e5150a24fd6b5220
saranheroic <sarann2302@gmail.com>
2024-10-01 20:07:43 +0530

    added constant files

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,13 +4,19 @@ import {hamburger} from "../assets/icons"
 
 const Nav = () => {
   return (
-    <header>
-        <nav>
+    <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'>
             <a href=''>
                 <img 
                 src={headerLogo}
+                alt='logo'
+                width={130}
+                height={29}
                 />
             </a>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+
+            </ul>
         </nav>
     </header>
   )
