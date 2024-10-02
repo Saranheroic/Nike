ceb9519b5ca4997028f87d2aabd7367c4f58675a
saranheroic <sarann2302@gmail.com>
2024-10-02 14:46:16 +0530

    s

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -53,7 +53,7 @@ const Hero = () => {
           className='object-contain relative z-10'
         />
 
-        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'> 
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
           {shoes.map((image, index) => (
             <div key={index}>
               <ShoeCard
