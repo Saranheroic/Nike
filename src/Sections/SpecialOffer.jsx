48b19251a4b0111c55709c8157fab51b28b8d207
saranheroic <sarann2302@gmail.com>
2024-10-03 14:42:21 +0530

    updated service card

--- src/Sections/SpecialOffer.jsx
+++ src/Sections/SpecialOffer.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { offer } from '../assets/images'
+import Button from '../Components/Button'
+import { arrowRight } from '../assets/icons'
+
+
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className='flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container'>
+      <div className="flex-1">
+        <img src={offer} width={773} height={687} className='object-contain w-full'/>
+      </div>
+      <div className="flex flex-1 flex-col">
+      
+
+        <h2 className='mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
+          
+          <span className='text-coral-red'> Special</span> offer
+          
+          </h2>
+        <p className='mt-4 info-text'>
+        Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.
+        </p>
+        <p className='mt-6 info-text'>
+        Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.
+        </p>
+
+        <div className='mt-11 flex flex-wrap gap-4'>
+           <Button label='Shop Now' iconURL={arrowRight}/>
+           <Button label="Learn more" backgroundColor="bg-white" borderColor="border-slate-gray" textColor="text-slate-gray" />
+            </div>
+        
+        
+    </div>
+    
+      
+      
+      
+    </section>
   )
 }
 
